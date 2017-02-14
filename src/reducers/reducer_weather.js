306a333fcfdf71bdbdf1ad570ac40045317ba794
xingjian <deric.pan@gmail.com>
2017-02-14 18:14:34 +0800

    minor

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,11 @@
-export default function (state=null, action) {
-  console.log('Action recieved', action)
+import {FETCH_WATHER} from '../actions/index'
+
+export default function (state=[], action) {
+  // console.log('Action recieved', action)
+
+  switch (action.type){
+    case FETCH_WATHER:
+      return [...state, action.payload.data]
+  }
   return state
 }
